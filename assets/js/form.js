c46990f16531697de28ced4a38905a62c8b15faa
Leena <leenacruz@gmail.com>
2024-05-18 13:23:01 -0700

    Created Submit Form

--- assets/js/form.js
+++ assets/js/form.js
@@ -2,3 +2,33 @@
 // localStorage
 //Prevent Default if needed
 // link to blog posts
+
+// Declare variables and define elementsof the form.
+//const FormEl = document.
+// use queryselector or getElementById?
+//const username = document.getElementById('username');
+
+const usernameInput = document.getElementById('username');
+const titleInput = document.getElementById('title');
+const contentInput = document.getElementById('content');
+const submitButton = document.getElementById('submitButton');
+
+
+submitButton.addEventListener('click', function (event) {
+event.preventDefault();
+
+
+const blogPost = {
+    username: username.value,
+    title: titleInput.value,
+    content: contentInput.value,
+};
+
+localStorage.setItem('blogPost', JSON.stringify(blogPost));
+
+//console.log(blogPost);
+// Redirect to blogpost pages
+location.href = "https://leenacruz.com/";
+
+});
+
