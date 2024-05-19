4b2b081dc2726eab445c384b504dae7b2af7f34a
Leena <leenacruz@gmail.com>
2024-05-18 19:02:11 -0700

    Add styles to css

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -12,7 +12,7 @@ const body = document.body;
 //const headerEL = document.querySelector('header');
 const containerEl = document.querySelector('container');
 const formEl = document.querySelector('form');
-const photoEl = document.querySelector('photo')
+const photoEl = document.querySelector('photo');
 
 // // Hero Image
 // const imgEL = document.createElement('img');
@@ -30,7 +30,8 @@ const photoEl = document.querySelector('photo')
 const imgEL = document.querySelector('img');
 //body.appendChild(photoEl)
 // body.appendChild(headerEL); // This works but erases the img, why?
-body.appendChild(imgEL);
+//body.appendChild(photoEl);
+// body.appendChild(imgEL)
 
 imgEL.setAttribute('src', 'assets/hero-image.jpg');
 // imgEL.setAttribute('style', 'margin: 10 px', 'border: 2 px');
