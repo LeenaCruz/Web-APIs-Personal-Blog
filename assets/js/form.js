26ecba21d205e62054816aecf70ec17c66650464
Leena <leenacruz@gmail.com>
2024-05-19 19:40:10 -0700

    Add form empty fields condition anb CSS styling

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,53 +1,24 @@
-//Define form submit 
-// localStorage
-//Prevent Default if needed
-// link to blog posts
-
-// Declare variables and define elementsof the form.
-//const FormEl = document.
-// use queryselector or getElementById?
-//const username = document.getElementById('username');
 
 const usernameInput = document.getElementById('username');
 const titleInput = document.getElementById('title');
 const contentInput = document.getElementById('content');
 const submitButton = document.getElementById('submitButton');
 
-
 submitButton.addEventListener('click', function (event) {
     event.preventDefault();
-
-    do while 
-    // Check if all the fields have info
-    // if (
-    //      usernameInput.value === " "; &&
-    //      titleInput.valu === " "; &&
-    //      contentInput.value === " ";
-
-    // ) {
-
-
-    // alert("Please fill all ther form fields");
-    //    break;
-
-    // }
-    // else {
-
-
+   if (usernameInput.value === "" ||
+    titleInput.value === "" ||
+    contentInput.value === ""
+    ) {
+   alert("Please fill all the form fields");
+   } else {
         const blogPost = {
             username: usernameInput.value,
             title: titleInput.value,
             content: contentInput.value,
         };
-
-
         localStorage.setItem('blogPost', JSON.stringify(blogPost));
-
-        console.log(blogPost);
-        // Redirect to blogpost pages * check target blank/new window
-        location.href = "https://leenacruz.com/";
-
-       // }
-
+       //console.log(blogPost);
+        location.href = "./blog.html"; // Redirect to blogpost pages * check target blank/new window
+      }
     });
-
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,33 +1,12 @@
 // Icon of dark mode could be here?
 
-
-// const headerEL = document.querySelector('.header');
-
-
-// const menuEl = document.createElement(headerEL);
-// document.body.appendChild(menuEl);
-
 // HEADER 
 const body = document.body;
-//const headerEL = document.querySelector('header');
 const containerEl = document.querySelector('container');
 const formEl = document.querySelector('form');
 const photoEl = document.querySelector('photo');
-
-// // Hero Image
-// const imgEL = document.createElement('img');
-
-
-// // body.appendChild(headerEL); // This works but erases the img, why?
-// body.appendChild(imgEL);
-
-
-// imgEL.setAttribute('src', 'assets/hero-image.jpg');
-// // imgEL.setAttribute('style', 'margin: 10 px', 'border: 2 px');
-
-
-
 const imgEL = document.querySelector('img');
+const blogPosts = document.querySelector('blockPosts')
 //body.appendChild(photoEl)
 // body.appendChild(headerEL); // This works but erases the img, why?
 //body.appendChild(photoEl);
